2243278223e80afe48c22fdbc8d81d0b137ee3bb
Roger Call <roger@codeparticle.com>
2017-04-15 12:38:48 -0700

    Refactored project structure

--- scripts/dlm.js
+++ scripts/dlm.js
@@ -2,16 +2,17 @@ $(function() {
 	var pull = $('#pull');
 	var menu = $('nav ul');
 	var menuHeight = menu.height();
-    $(pull).on('click', function(e) {
-	    e.preventDefault();
-	    menu.slideToggle();
-    });
-    $(window).resize(function(){
-    		var w = $(window).width();
-    		if(w > 320 && menu.is(':hidden')) {
-    			menu.removeAttr('style');
-    		}
-  	});
+
+  $(pull).on('click', function(e) {
+    e.preventDefault();
+    menu.slideToggle();
+  });
+  $(window).resize(function(){
+      var w = $(window).width();
+      if(w > 320 && menu.is(':hidden')) {
+        menu.removeAttr('style');
+      }
+  });
 });
 
 $(document).ready(function(){
